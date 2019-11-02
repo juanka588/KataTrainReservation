3ec6e5868d722598453dacb318b5e3e400426de6
Antoine Rosenbach <github@rbch.ovh>
2019-11-02 19:02:26 +0100

    Train seats is the union of the seats of all coaches

--- Java/src/main/java/coltrain/Train.java
+++ Java/src/main/java/coltrain/Train.java
@@ -3,39 +3,45 @@ package coltrain;
 import coltrain.api.models.Seat;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.toList;
+
 public class Train {
     private static final double CAPACITY_THRESHOLD = 0.70;
-    private final List<Seat> seats;
     private HashMap<String, Coach> coaches;
 
     public Train(final List<Seat> seats) {
         this.coaches = new HashMap<>();
 
-        for (Seat seat : seats){
+        for (Seat seat : seats) {
             final String coachName = seat.getCoachName();
-            if(!coaches.containsKey(coachName)){
+            if (!coaches.containsKey(coachName)) {
                 coaches.put(coachName, new Coach(coachName));
             }
             coaches.get(coachName).addSeat(seat);
         }
 
-        this.seats = seats;
     }
 
     public List<Seat> getSeats() {
-        return this.seats;
+        return this.coaches
+                .values()
+                .stream()
+                .map(Coach::getSeats)
+                .flatMap(Collection::stream)
+                .collect(toList());
     }
 
     public int getReservedSeats() {
-        return Math.toIntExact(this.seats.stream().filter(s -> !s.getBookingRef().isEmpty()).count());
+        return Math.toIntExact(this.getSeats().stream().filter(s -> !s.getBookingRef().isEmpty()).count());
     }
 
     public int getMaxSeat() {
-        return this.seats.size();
+        return this.getSeats().size();
     }
 
     public boolean doNotExceedCapacityThreshold(final int requestedSeatsCount) {
