f8505e7087693a29aabf1c241c3bf8ec51479ca5
Simon <skempf@salesforce.com>
2020-05-26 17:53:08 +0200

    Reservation with some seats requested + TrainTopology created

--- Java/src/test/java/coltrain/WebTicketManagerTest.java
+++ Java/src/test/java/coltrain/WebTicketManagerTest.java
@@ -8,9 +8,15 @@ public class WebTicketManagerTest {
 
     @Test
     public void reserve_givenZeroSeatsRequested_itShouldReturnNoSeats() {
-        final WebTicketManager sut = new WebTicketManager(new FakeTrainDataService(), new FakeBookingReferenceService());
+        final WebTicketManager sut = new WebTicketManager(new FakeTrainDataService(TrainTopology.TRAIN_WITH_TWO_SEATS_NOT_RESERVED), new FakeBookingReferenceService());
         final String reservation = sut.reserve("express2000", 0);
         assertEquals("{\"trainId\": \"express2000\",\"bookingReference\": \"bookingReference\",\"seats\":[]}", reservation);
     }
 
+    @Test
+    public void reserve_givenTwoSeatsRequested_itShouldReturnTwoSeats() {
+        final WebTicketManager sut = new WebTicketManager(new FakeTrainDataService(TrainTopology.TRAIN_WITH_SIX_SEATS_NOT_RESERVED), new FakeBookingReferenceService());
+        final String reservation = sut.reserve("express2000", 2);
+        assertEquals("{\"trainId\": \"express2000\",\"bookingReference\": \"bookingReference\",\"seats\":[\"1A\", \"2A\"]}", reservation);
+    }
 }
