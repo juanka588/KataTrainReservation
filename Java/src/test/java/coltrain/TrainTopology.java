97ab38e7e26e581b7b6a5a7869460569434e5bf3
Simon <skempf@salesforce.com>
2020-05-29 15:00:40 +0200

    Second business rule tested and is bugged + most of code is covered!

--- Java/src/test/java/coltrain/TrainTopology.java
+++ Java/src/test/java/coltrain/TrainTopology.java
@@ -16,4 +16,15 @@ public class TrainTopology {
                     "\"6A\": {\"booking_reference\": \"\", \"seat_number\": \"6\", \"coach\": \"A\"}" +
                     "}" +
                     "}";
+
+    public static final String TRAIN_TWO_COACHES_SIX_SEATS_AND_ONE_SEAT_RESERVED =
+            "{\"seats\": " +
+                    "{\"1A\": {\"booking_reference\": \"bookingRef\", \"seat_number\": \"1\", \"coach\": \"A\"}," +
+                    "\"2A\": {\"booking_reference\": \"\", \"seat_number\": \"2\", \"coach\": \"A\"}," +
+                    "\"3A\": {\"booking_reference\": \"\", \"seat_number\": \"3\", \"coach\": \"A\"}," +
+                    "\"4B\": {\"booking_reference\": \"\", \"seat_number\": \"4\", \"coach\": \"B\"}," +
+                    "\"5B\": {\"booking_reference\": \"\", \"seat_number\": \"5\", \"coach\": \"B\"}," +
+                    "\"6B\": {\"booking_reference\": \"\", \"seat_number\": \"6\", \"coach\": \"B\"}" +
+                    "}" +
+                    "}";
 }
--- Java/src/test/java/coltrain/WebTicketManagerTest.java
+++ Java/src/test/java/coltrain/WebTicketManagerTest.java
@@ -1,5 +1,6 @@
 package coltrain;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -25,6 +26,14 @@ public class WebTicketManagerTest {
         final WebTicketManager sut = new WebTicketManager(new FakeTrainDataService(TrainTopology.TRAIN_SIX_SEATS_NOT_RESERVED), new FakeBookingReferenceService());
         final String reservation = sut.reserve("express2000", 5);
         assertEquals("{\"trainId\": \"express2000\", \"bookingReference\": \"\", \"seats\":[]}",reservation);
+    }
+
+    @Ignore("business rule is not respected")
+    @Test
+    public void reserve_givenTwoSeatsRequested_itShouldReturnSeatsInTheSameCoach() {
+        final WebTicketManager sut = new WebTicketManager(new FakeTrainDataService(TrainTopology.TRAIN_TWO_COACHES_SIX_SEATS_AND_ONE_SEAT_RESERVED), new FakeBookingReferenceService());
+        final String reservation = sut.reserve("express2000", 3);
+        assertEquals("{\"trainId\": \"express2000\", \"bookingReference\": \"\", \"seats\":[\"4B\", \"5B\", \"6B\"]}",reservation);
     }
 
 }
